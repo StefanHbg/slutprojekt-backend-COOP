54952d241c5fe0427535dc6b754ddc54f0a0b660
StefanHbg <70539225+StefanHbg@users.noreply.github.com>
2021-05-21 16:10:28 +0200

    added orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const { create } = require('../modules/Order')
 const Order = require('../modules/Order')
+const Product = require('../modules/Product')
 const router = express.Router()
 
 //const router = new Router()
 
 router.post('/', async (req, res) => {
     console.log(req.body);
+    const orderSum = req.body.items.reduce((acc, id) => {
+        const product = Product.findById(id)
+        console.log(product);
+        return acc + product.price;
+    })
+    console.log(orderSum);
     const newOrder = new Order({
         timeStamp: Date.now(),
         status: 'inProcess',
         items: [req.body.items],
-        orderValue: ''
+        orderValue: orderSum
     })
     newOrder.save((err) => {
         if (err) res.json(err)
